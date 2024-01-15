b95e7a29e1df4f695107688b82a9dd663d1c53d3
Casper Post <cpost@f1r5s18.codam.nl>
2024-01-15 11:56:13 +0100

    merged with backend svos

--- frontend/components/chat/SettingsTab/settings.tsx
+++ frontend/components/chat/SettingsTab/settings.tsx
@@ -25,34 +25,8 @@ export function SettingsTab({ setUserName, chatSocket, userName, intraId}
 	: React.JSX.Element
 {
 
-	// The New user name of the user
-	const [newUserName, setNewUserName] = useState<string>("");
-
   	return (
         <div>
-			<h2 className="h4_font font-bold pb-4">Change username</h2>
-
-        	<SimpleForm
-                onSubmit={(event: React.FormEvent<HTMLFormElement>) => changeUserName(event, newUserName, setUserName, chatSocket, userName, intraId, setNewUserName)}
-                content={
-                  <div className="flex">
-                    <InputSimple 
-                      input={newUserName} 
-                      setInput={setNewUserName}
-                      placeholder={"Type new username here ..."}
-                    />
-                    <StandardButton 
-						text={"Change"}
-						buttonStyle={"border-white border-[1px] hover:bg-white/20"}
-					/>
-                  </div>
-                }
-            />
-
-			<p className='py-3 border-b-[1px]'>
-				<span className="font-bold">Current username: </span>
-				{userName}
-			</p>
         </div>
   )
 }
